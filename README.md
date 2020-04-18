d4a073a1dfbb8f80e16aaf4bdcfa6628eac38584
huameicc <huameicc@163.com>
2020-04-19 00:04:47 +0800

    echo * when input passwd.

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-A method to generate passwords: Use a source_key, a domain and a salt to 
-generate password. 
+# A method to generate passwords 
 
-You could only remember one source_secret in your brain. When You want to get 
-a new password, just provide a new domain or salt.  
+### Use a source_key, a domain and a salt to generate passwords reproducibly. 
 
-domain: could be a website name or something.
-salt: could be username or something.
+- You should only remember one source_key in your brain. When you want to get a new password, just provide a new domain or salt.
 
-Just keep domains and salts in a txt, and store it in a cloud or something. Or 
-you could even write it down with your pen, keep it under your pillow.
+- You can keep domains and salts in a text, and store it in a cloud or somewhere. Or you could just write it down with your pen, and keep it under your pillow.
 
-When You forget a password, just re_calculate it with your source_key along with 
-domain and salt.
+- When You forget a password, just re-calculate it with your source_key along with domain and salt.
+
+### Algorithm: 
+
+```
+URLSAFE_BASE64(SHA256(SHA256(source_key), domain, salt))
+```
+*Note: [Check out the One-Way feature of hash-digest.]*
 
 
-Algorithm: 
-  URLSAFE_BASE64(SHA256(SHA256(source_key), domain, salt))
 
-Note: [Check out the One-Way feature of hash-digest.]
-
+[^domain]: could be a website name or something.
+[^salt]: could be user_name or something.
 
 
 
--- pass_generator.py
+++ pass_generator.py
@@ -1,7 +1,14 @@
-import base64, hashlib
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 
-def get_pass(passwd, domain, salt, size=16, encoding='utf_8'):
+import base64, hashlib, msvcrt
+
+
+UTF8 = 'utf-8'
+
+
+def get_pass(passwd, domain, salt, size=16, encoding=UTF8):
     """
     pass: your source secret to generate a password. Don't tell any body,
         best to just keep in your mind. And Dont't forget it, or you will 
@@ -34,16 +41,43 @@ def input_size(info):
             a = int(_s)
             if a < 1 or a > 44:
                 info = 'size need to be around [1, 44], input size again:'
+                continue
             return a
         except ValueError:
             info = 'size need to be a number, input again:' 
+            
+
+def input_pwd(info):
+    """safe input passwd, echo * but no real alpha."""
+    # print(info, end='')
+    for b in info.encode():
+        msvcrt.putch(bytes([b,]))
+    chars = []   
+    while True:  
+        try:  
+            ch = msvcrt.getch().decode()  
+            if ch in '\r\n':            
+                 break   
+            elif ch == '\b':  
+                 if chars:    
+                     del chars[-1]   
+                     msvcrt.putch(b'\b') 
+                     msvcrt.putch(b' ')
+                     msvcrt.putch(b'\b')                    
+            else:  
+                chars.append(ch)  
+                msvcrt.putch(b'*')  
+        except:  
+            return input("Warning - input will not be hided: ")  
+    print()
+    return ''.join(chars) 
         
     
 if __name__ == '__main__':
     password = get_pass(domain = input('input website domain or name: '),
                    salt = input('input user name or salt: '),
+                   passwd = input_pwd('input your source secret: '),
                    size = input_size("input password_size, press 'enter'"
-                   + "to use 16: ") or 16,
-                   passwd = input('input your source secret: '))
+                   + "to use 16: ") or 16)
     print('Your Password Is: %s' %password)
     
